72ab940f93b78cfc2286d5e378115a5447c22a59
Fivice <wubin_oct@163.com>
2020-07-28 00:34:17 +0800

    thymeleaf整合

--- pvf-web/src/main/java/top/fivice/pvf/web/controller/DemoController.java
+++ pvf-web/src/main/java/top/fivice/pvf/web/controller/DemoController.java
@@ -1,14 +1,14 @@
 package top.fivice.pvf.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("index")
+@Controller
 public class DemoController {
-    @GetMapping("hello")
+    @GetMapping("index")
     public String test(){
-        return "hello spring boot";
+        return "index";
     }
 }
